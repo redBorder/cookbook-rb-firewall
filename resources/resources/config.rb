9b3cb079dba0ad9efd615f5f353d65337d30ce18
Pablo Pérez <pperez@redborder.com>
2025-06-10 12:38:13 +0100

    Added properties

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -8,5 +8,7 @@ actions :add, :remove
 default_action :add
 
 attribute :user, kind_of: String, default: 'firewall'
+property :vault_sensors, Array, required: false
+property :vault_sensor_in_proxy_nodes, Array, required: false
 property :sync_ip, String, required: false
 property :ip_addr, String, required: false
