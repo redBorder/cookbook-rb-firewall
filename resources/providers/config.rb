05085e9a3909a1969700e0254d5bb22b9c5153ef
vimesa <vimesa@redborder.com>
2026-03-04 14:12:29 +0000

    Fix lint

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -178,11 +178,11 @@ action :add do
       # Compare public
       public_runtime = `firewall-cmd --zone=public --list-rich-rules`.strip
       public_permanent = `firewall-cmd --permanent --zone=public --list-rich-rules`.strip
-  
+
       # Compare libvirt
       libvirt_runtime = `firewall-cmd --zone=libvirt --list-ports`.strip
       libvirt_permanent = `firewall-cmd --permanent --zone=libvirt --list-ports`.strip
-  
+
       # If there is a difference in any of the areas: reload
       (public_runtime != public_permanent) || (libvirt_runtime != libvirt_permanent)
     end
